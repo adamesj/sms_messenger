fdb22bf86af789b0ccb5c93c6867bfcfcc21179d
Jimmy Adames <jimmy.adames@gmail.com>
2016-09-22 11:55:02 -0400

    further configuration for deployment

--- main.rb
+++ main.rb
@@ -3,6 +3,13 @@ require 'twilio-ruby'
 require 'sinatra'
 require 'dotenv'
 
+configure do
+  DB = Sequel.connect(ENV['DATABASE_URL'] || 'sqlite://database.db')
+
+  gem 'sqlite3', :group => 'development'
+  gem 'pg', :group => 'production'
+end
+
 account_sid = 'ACc2833364b27ec907833f5075df828a0d'
 auth_token = '4746ca9cd0c60159e9409a5e3e88b608'
 
